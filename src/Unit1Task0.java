7751221fac4afe84b0c01d977cab9ac382db27d3
alexey1902 <alesha_1902@mail.ru>
2021-07-09 14:31:12 +0300

    Initial commit

---
+++ src/Unit1Task0.java
@@ -0,0 +1,42 @@
+public class Unit1Task0 {
+    public static void main(String[] args){
+        int month = 6;
+        int day = 1;
+        System.out.println("month + day = " + (month+day));
+        System.out.println("month - day = " + (month-day));
+        System.out.println("month % day = " + (month%day));
+        System.out.println("month * day = " + (month*day));
+        System.out.println("month / day = " + (month/day));
+        System.out.println("month += day = "+ (month+=day)); month = 6;
+        System.out.println("month -= day = "+ (month-=day)); month = 6;
+        System.out.println("month %= day = "+ (month%=day)); month = 6;
+        System.out.println("month *= day = "+ (month*=day)); month = 6;
+        System.out.println("month++ = " + ++month);          month = 6;
+        System.out.println("month-- = " + --month);          month = 6;
+
+        System.out.println("month | day = " + (month|day));
+        System.out.println("month & day = " + (month&day));
+        System.out.println("month ^ day = " + (month^day));
+        System.out.println("month >> day = " + (month>>day));
+        System.out.println("month >>= day = " + (month>>=day));
+        System.out.println("month >>> day = " + (month>>>day));
+        System.out.println("month >>>= day = " + (month>>>=day));
+        System.out.println("month << day = " + (month<<day));
+        System.out.println("month <<= day = " + (month<<=day));
+        System.out.println("month |= day = "+ (month|=day));
+        System.out.println("month &= day = "+ (month&=day));
+        System.out.println("month ^= day = "+ (month^=day));
+        System.out.println("month *= day = "+ (month*=day));
+
+        System.out.println("month < day = "  +  (month<day));
+        System.out.println("month > day = "  +  (month>day));
+        System.out.println("month == day = " + (month==day));
+        System.out.println("month >= day = " + (month>=day));
+        System.out.println("month <= day = " + (month<=day));
+        System.out.println("month != day = " + (month!=day));
+        boolean month2 = false;
+        boolean day2 = true;
+        System.out.println("month2 || day2 = " + (month2||day2));
+        System.out.println("month2 & day2 = " + (month2&day2));
+    }
+}
---
+++ src/Unit1Task4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Unit1Task4 {
+    public static void main(String[] args) {
+        System.out.print("Введите 3 целых числа: ");
+        int [] numbers = new int[3];
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        for(int i=0; i<3; i++) {
+           numbers[i] = scan.nextInt();
+           if(numbers[i]>0) count++;
+        }
+        System.out.print("Количество положительных чисел - " + count);
+    }
+}
