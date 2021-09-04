3f24689f7b0ccb4398ad8a1c3003ca9c85d01bc3
alexey1902 <alesha_1902@mail.ru>
2021-09-04 19:46:31 +0300

    Unit14_15

---
+++ src/ShopUnit14_15/Product.java
@@ -0,0 +1,39 @@
+package ShopUnit14_15;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Product implements Serializable {
+    private final int id;
+    private int price;
+    private String name;
+    private final LocalDateTime dateTime;
+
+    public Product(int id, int price, String name) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public int getId() { return id; }
+
+    public int getPrice() { return price; }
+
+    public String getName() { return name; }
+
+    public LocalDateTime getDateTime() { return dateTime; }
+
+    public void setPrice(int price) { this.price = price; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void show(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
+        System.out.println("Наименование: " + this.getName());
+        System.out.println("Цена: " + this.getPrice());
+        System.out.println("ID: " + this.getId());
+        System.out.println("Добавлен: " + dtf.format(this.getDateTime()) + '\n');
+    }
+}
