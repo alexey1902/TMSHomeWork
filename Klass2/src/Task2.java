d11f3fbc6a4d576b0936aa8f49d84454d117d933
Aleksei-Prudnikoff <87182664+alexey1902@users.noreply.github.com>
2021-11-17 14:06:45 +0300

    spring core calculator using 3 beans

--- Klass2/src/Task2.java
+++
@@ -1,7 +0,0 @@
-public class Task2 {
-        public static void main(String args[]) {
-            for (int i = 6; i > 0; i--) {
-                System.out.print(i + " ");
-            }
-        }
-    }
