d1f7c1f6e41584b9c445672db2107254a8549da3
Aleksei-Prudnikoff <87182664+alexey1902@users.noreply.github.com>
2021-11-17 14:07:22 +0300

    spring core calculator using 3 beans

--- Unit2/src/Unit2Task6.java
+++
@@ -1,9 +0,0 @@
-public class Unit2Task6 {
-    public static void main(String[] args) {
-        int i = 2;
-        do{
-            System.out.println(i+" ");
-            i+=2;
-        }while(i!=102);
-    }
-}
